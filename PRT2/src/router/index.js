885d8f518f4208ce5402511bcca21d663facac8c
Duarte Cabrita <cabritagamer1@gmail.com>
2023-06-17 22:05:09 +0100

    checkout page front end done C:
    with responsive also done C:

--- PRT2/src/router/index.js
+++ PRT2/src/router/index.js
@@ -6,7 +6,9 @@ const router = createRouter({
         { path: "/", component: () => import("../views/HomePage.vue")},
         { path: "/user", component: () => import("../views/UserPage.vue")},
         { path: "/item/:id", component: () => import("../views/ItemPage.vue")},
+        { path: "/checkout", component: () => import("../views/Checkout.vue")},
         { path: "/:pathMatch(.*)*", component: () => import("../views/PageNotFound.vue") },
     ]
 })
+
 export default router;
---
+++ PRT2/src/views/Checkout.vue
@@ -0,0 +1,386 @@
+<!--Checkout Page to complete order-->
+
+<template>
+
+    <div class="overall">
+
+        <!--Logo-->
+
+        <div class="logo">
+            <img src="../assets/logo3.png" alt="">
+        </div>
+
+        <!--Checkout Container-->
+
+        <div class="checkoutPage">
+            <h1>Checkout</h1>
+            <form>
+                <div class="formGroup">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" v-model="name" required>
+                </div>
+                <div class="formGroup">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" v-model="email" required>
+                </div>
+                <div class="formGroup">
+                    <label for="email">Address:</label>
+                    <input type="address" id="address" v-model="address" required>
+                </div>
+                <div class="formGroup">
+                    <label for="card-number">Card Number:</label>
+                    <input type="text" id="card-number" v-model="cardNumber" required>
+                </div>
+                <div class="formGroup">
+                    <div class="expiryCVV">
+                        <div class="expiry">
+                            <label for="expiryDate">Expiry Date:</label>
+                            <input type="text" id="expiryDate" v-model="formattedExpiryDate" required>
+                        </div>
+                        <div class="cvv">
+                            <label for="cvv">CVV:</label>
+                            <input type="text" id="cvv" v-model="cvv" required>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Cart Items-->
+
+                <div class="itemListContainer">
+                    <h2>Items:</h2>
+                    <div class="itemList">
+                        <ul>
+                            <li v-for="item in cartItemsGrouped" :key="item.id">
+                                <span v-if="item.quantity >= 1">{{ item.quantity }}x </span>{{ item.name }} - {{ item.price }}€
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="totalPrice">
+                        <p>Total: <span>{{ cartTotal }}€</span></p>
+                    </div>
+                </div>
+                <button class="submitBtn" type="submit">Submit</button>
+            </form>
+        </div>
+
+        <!--Go Back Button-->
+
+        <div class="goBackBtn">
+            <button class="backButton">
+                <span>&larr;</span>
+                <span class="goBackText">Go Back</span>
+            </button>
+        </div>
+    </div>
+
+</template>
+  
+<script>
+
+  export default{
+    data() {
+      return {
+        name: '',
+        email: '',
+        address: '',
+        cardNumber: '',
+        expiryDate: '',
+        cvv: '',
+        cartItems: [
+            { id: 1, name: 'Adventure Note 1', price: 10 },
+            { id: 2, name: 'Dangozamurai Full Bundle', price: 10 },
+            { id: 3, name: 'The One Full', price: 15 },
+            { id: 3, name: 'The One Fu', price: 15 },
+            { id: 3, name: 'The One Fu', price: 15 },
+            { id: 3, name: 'The One Fu', price: 15 },
+            { id: 3, name: 'The One Fu', price: 15 },
+            { id: 4, name: 'The One Fu', price: 15 },
+            { id: 5, name: 'The One Fu', price: 15 },
+        ],
+      };
+    },
+
+    computed: {
+        formattedExpiryDate: {
+            get() {
+            return this.expiryDate;
+            },
+            set(value) {
+            const formattedValue = value.replace(/\D/g, '');
+            let formatted = '';
+            for (let i = 0; i < formattedValue.length; i++) {
+                formatted += formattedValue[i];
+                if (i === 1) {
+                formatted += '/';
+                }
+            }
+            this.expiryDate = formatted;
+            }
+        },
+
+        cartItemsGrouped() {
+            const groupedItems = this.cartItems.reduce((acc, item) => {
+            const existingItem = acc.find((i) => i.id === item.id);
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                acc.push({ ...item, quantity: 1 });
+            }
+            return acc;
+        }, []);
+            return groupedItems;
+        },
+
+        cartTotal() {
+            return this.cartItems.reduce((total, item) => total + item.price, 0);
+        },
+    },
+};
+
+</script>
+  
+<style scoped>
+
+
+/*Overall*/
+
+.overall{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 200px;
+    margin-top: 55px;
+}
+
+/*Logo*/
+
+.logo{
+    margin-right: 20px;
+    justify-content: center;
+    align-items: center;
+}
+
+.logo img{
+    width: 500px;
+    max-width: 100%;
+    height: auto;
+}
+
+/*Checkout Content*/
+
+.checkoutPage{
+    max-width: 600px;
+    padding: 20px;
+    background-color: rgb(58, 56, 53);
+    border-radius: 10px;
+    border: 2px solid white;
+    color: white;
+}
+
+.checkoutPage h1{
+    text-align: center;
+    margin-bottom: 20px;
+    color: #ffffff;
+    text-shadow: 0 0 2px #000000, 0 0 2px #000000;
+}
+
+.checkoutPage form{
+    display: grid;
+    gap: 20px;
+}
+
+.formGroup{
+    display: flex;
+    flex-direction: column;
+}
+
+.formGroup label{
+    font-weight: bold;
+    text-shadow: 0 0 2px #000000, 0 0 2px #000000;
+    margin-left: 20px;
+    margin-bottom: 2px;
+}
+
+.formGroup input[type="text"],
+input[type="email"],
+input[type="address"],
+input[type="text"]:focus,
+input[type="email"]:focus,
+input[type="address"]:focus {
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    background-color: #f5f5f5;
+    font-size: 16px;
+    margin-top: 10px;
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.expiryCVV{
+    display: flex;
+    flex-direction: row;
+    margin-left: auto;
+    margin-right: auto;
+    width: 69%;
+    text-align: center;
+}
+
+.expiry,
+.cvv{
+    flex: 1;
+}
+
+.expiry input[type="text"],
+.cvv input[type="text"]{
+    width: 100%;
+    padding: 5px;
+    border: none;
+    border-radius: 5px;
+    background-color: #f5f5f5;
+    font-size: 14px;
+}
+
+.submitBtn{
+    padding: 10px 20px;
+    background-color: rgb(136, 117, 94);
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 18px;
+    width: 65%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 10px;
+}
+
+.submitBtn:hover{
+    background-color: rgb(204, 189, 171);
+    color: black;
+}
+
+#cvv{
+    width: 20%;
+    margin-left: 15px;
+}
+
+#expiryDate{
+    width: 20%;
+    margin-left: 15px;
+}
+
+/*Items Review*/
+
+.itemListContainer{
+    max-width: 500px;
+    padding: 20px;
+    background-color: rgb(95, 93, 88);
+    border-radius: 10px;
+    color: white;
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.itemList{
+    height: 100px;
+    margin-top: 5px;
+    overflow-y: auto;
+    scrollbar-width: thin;
+    width: 100%;
+
+}
+
+.itemListContainer h2{
+    color: white;
+    font-size: 20px;
+    margin-top: 0;
+    text-align: center;
+}
+
+.itemList ul{
+    list-style-type: none;
+    padding-left: 0;
+}
+
+.itemList li{
+    font-size: 17px;
+    margin-top: 10px;
+}
+
+.itemList li span{
+    font-weight: bold;
+
+}
+
+.totalPrice p{
+    font-weight: bold;
+    text-align: center;
+}
+
+/*Go back Button*/
+
+.goBackText{
+    margin-left: 10px;
+}
+.goBackBtn{
+    position: absolute;
+    top: 10px;
+    left: 20px;
+}
+
+
+.backButton{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+    background-color: rgb(58, 56, 53);
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    width: 100%;
+}
+
+.backButton:hover{
+    color:white;
+}
+
+/*Responsive*/
+
+@media screen and (max-width: 1000px) {
+
+    .goBackBtn{
+        position: static;
+        margin-top: -150px;
+        margin-bottom: 20px;
+    }
+    .checkoutPage{
+        padding: 20px;
+        margin-top: -120px;
+    }
+
+    form{
+        gap: 10px;
+    }
+
+    .overall{
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .logo{
+        margin-right: 0;
+    }
+
+    .logo img{
+        width: 300px;
+    }
+}
+
+  </style>
+  
