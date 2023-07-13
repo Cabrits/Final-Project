2ec1ae410a79706431ea0d115ca5946104d0e5fd
Duarte Cabrita <cabritagamer1@gmail.com>
2023-07-13 01:19:19 +0100

    final slider
    now its possible to change it by dragging and its automated
    
    used swiper.js

--- front-end/src/components/Slider.vue
+++ front-end/src/components/Slider.vue
@@ -2,25 +2,27 @@
 
 <template>
 
-  <!--Overall Slider-->
+  <!-- Overall Slider -->
 
   <div class="slideNews">
 
-    <!--Images-->
+    <!-- Images -->
 
     <div class="sliderWrapper">
-      <div class="slider">
-        <img id="slide1" src="@/assets/news1.jpg" />
-        <img id="slide2" src="@/assets/news2.jpg" />
-        <img id="slide3" src="@/assets/news4.jpg" />
-        <img id="slide4" src="@/assets/news3.jpg" />
-      </div>
-
-      <!--Navigation circles-->
-
-      <div class="sliderNav">
-        <a v-for="(slide, index) in slides" :key="index" :href="`#slide${index + 1}`" :class="{ active: currentIndex === index }" @click.prevent="changeSlide(index)"></a>
-      </div>
+      <swiper :rewind="true" :centeredSlides="true" :autoplay="{ delay: 3000, disableOnInteraction: false}" :pagination="{ clickable: true}" :modules="modules" class="slider mySwiper">
+        <swiper-slide>
+          <img id="slide1" src="@/assets/news1.jpg" />
+        </swiper-slide>
+        <swiper-slide>
+          <img id="slide2" src="@/assets/news2.jpg" />
+        </swiper-slide>
+        <swiper-slide>
+          <img id="slide3" src="@/assets/news4.jpg" />
+        </swiper-slide>
+        <swiper-slide>
+          <img id="slide4" src="@/assets/news3.jpg" />
+        </swiper-slide>
+      </swiper>
     </div>
   </div>
 
@@ -28,62 +30,26 @@
 
 <script>
 
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination } from 'swiper/modules';
+
 //  Export the component
 export default{
   name: "Slider",
   //  Data for the component (slides and current index)
-  data() {
+
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  
+  setup() {
     return {
-      slides: [1, 2, 3, 4],
-      currentIndex: 0,
-      autoRotationInterval: null, // Store the interval reference
+      modules: [Autoplay, Pagination],
     };
-  },
-
-  //  Methods for the component (change slide, start and stop auto rotation)
-  methods: {
-    //  Change the slide by clicking on the navigation circles
-    changeSlide(index) {
-      this.currentIndex = index;
-      //  Scroll to the selected slide
-      const slider = document.querySelector(".slider");
-      slider.scrollTo({
-        left: slider.offsetWidth * index,
-        behavior: "smooth",
-      });
-    },
-
-    //  Start the auto rotation of the slides (every 10 seconds)
-    startAutoRotation() {
-      this.autoRotationInterval = setInterval(() => {
-        this.currentIndex = (this.currentIndex + 1) % this.slides.length;
-        this.changeSlide(this.currentIndex);
-      }, 10000); // Change slide every 10 seconds
-    },
-
-    //  Stop the auto rotation of the slides
-    stopAutoRotation() {
-      clearInterval(this.autoRotationInterval);
-      this.autoRotationInterval = null; // Clear the interval reference
-    },
-  },
-
-  // mounted lifecycle hook to start the auto rotation on component mount
-  mounted() {
-    this.startAutoRotation();
-  },
-
-  // beforeUnmount lifecycle hook to stop the auto rotation on component unmount
-  beforeUnmount() {
-    this.stopAutoRotation();
-  },
-
-  //  beforeRouteLeave lifecycle hook to stop the auto rotation when the user leaves the page
-  created() {
-    this.$router.beforeEach((to, from, next) => {
-      this.stopAutoRotation();
-      next();
-    });
   },
 };
 
@@ -126,47 +92,27 @@ export default{
 
 /* Slider "buttons" */
 
-.sliderNav{
-    display: flex;
-    column-gap: 1rem;
-    position: absolute;
-    bottom: 1.25rem;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 1;
-}
-
-.sliderNav a{
-    width: 0.6rem;
-    height: 0.6rem;
-    border-radius: 50%;
-    background-color: #000000;
-    transition: opacity ease 250ms;
-}
-
-.sliderNav a:hover{
-    opacity: 1;
-}
-
-.sliderNav a.active{
-    opacity: 1;
-    background-color: #edd3ae;
-}
-
-.sliderNav:focus{
-    color: white;
-}
 
 @media screen and (max-width: 500px){
   .slideNews{
       padding: 2rem;
   }
+}
 
+</style>
 
-  .sliderNav a{
-      width: 0.4rem;
-      height: 0.4rem;
-  }
+<style>
+
+.swiper-pagination-bullet{
+  background-color: #000000; /* Replace with your desired color */
+  width: 11px;
+  height: 11px;
+  opacity: 1;
+  margin: 0 10px!important;
+}
+
+.swiper-pagination-bullet-active{
+  background-color: #e7b182; /* Replace with your desired active color */
 }
 
 </style>
